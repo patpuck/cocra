375c825a6138b29391156a87621b01fd49c2e132
patpuck <davidlynch707@gmail.com>
2026-06-19 21:41:23 -0700

    localpositioningdardar

--- pp/util.lua
+++ pp/util.lua
@@ -4,6 +4,8 @@ function pp.ren(n) return pretty.render(pretty.pretty(n)) end
 function pp.round(n, pre) pre = pre or 1 return math.floor(n/pre+0.5)*pre end
 function pp.mVec(M) return vector.new(M[1][1], M[1][2], M[1][3]) end
 -- function pp.vColumn(v) return matrix end
+function pp.tVec(T) return vector.new(T.x, T.y, T.z) end
+function pp.tQuat(T) return quaternion.new(pp.tVec(T.v), T.a) end
 function pp.fromPolar(r, theta, y) return vector.new( r * math.cos(theta), y or 0, -r * math.sin(theta) ) end
 function pp.clamp(n, min, max) return math.max(min, math.min(n, max)) end
 function pp.bound(n, bound, center) center = center or 0 return math.max(-bound + center, math.min(n, bound + center)) end
@@ -102,34 +104,58 @@ end
 
 -- :::: net foundation :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-function pp.net.postToNetwork(header)
-    local packet = {
-        protocol = pp.net.PROTOCOL,
-        sender = pp.net.COMPID,
-        header = header or "UPDATE",
-        status = pp.net.STATUS,
-        timeSent = os.epoch("utc"),
-        payload = pp.net.myData
+function pp.net.postToNetwork(header, data)
+    local packet    = {
+        protocol    = pp.net.PROTOCOL,
+        sender      = pp.net.COMPID,
+        header      = header or "UPDATE",
+        status      = pp.net.STATUS,
+        timeSent    = pp.t(),
+        payload     = data or pp.net.myData
         -- flags = pp.net.FLAGS
     }
 
-    pp.net.networkState[packet.sender] = packet.payload
+    -- if header and (header == "UPDATE") then 
+    --     pp.net.networkState[packet.sender] = packet.payload 
+    -- end
+    pp.net.getNetwork(packet.sender, packet)
     -- pp.print("broadcasting")
     rednet.broadcast(packet, pp.net.PROTOCOL)
 end
 
 -- pp.print({asdf, "gar"})
 
-function pp.net.getNetwork()
-    local id, packet = rednet.receive(pp.net.PROTOCOL, 0.05)
+function pp.net.getNetwork(idfake, packetfake)
+
+    local id, packet = 0, 0
+
+    if idfake and packetfake then
+        id, packet = idfake, packetfake
+    else
+        id, packet = rednet.receive(pp.net.PROTOCOL, 0.05)
+    end
+
     if not (packet and type(packet) == "table") then return end
+
     if packet.header == "IMNEWHERE" then
+
         pp.print("comp #"..packet.sender.." has joined the network.")
-    end
-    if packet.header == "UPDATE" then
+        pp.net.postToNetwork()
+        return
+        
+    elseif packet.header == "UPDATE" then
+
         pp.net.networkState[packet.sender] = packet.payload
-    -- elseif packet.header == ""
+        return
+
+    elseif packet.header == "GLOBAL" then
+
+        -- pp.print("GLOBALLL" )
+        pp.net[packet.globalKey] = packet.payload
+        return
+
     end
+
 end 
 
 -- :::: patnet api     :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -141,6 +167,23 @@ end
 function pp.net.updateKey(k, v)
     pp.net.myData[k] = v
     pp.net.postToNetwork()
+end
+
+function pp.net.updateGlobalKey(k, v)
+    -- pp.print("GLOBALING")
+    local packet = {
+        protocol    = pp.net.PROTOCOL,
+        sender      = pp.net.COMPID,
+        header      = "GLOBAL",
+        status      = pp.net.STATUS,
+        timeSent    = pp.t(),
+        payload     = v,
+        globalKey   = k
+        -- flags = pp.net.FLAGS
+    }
+    pp.net.getNetwork(packet.sender, packet)
+
+    rednet.broadcast(packet, pp.net.PROTOCOL)
 end
 
 -- :::: sublevel stuff :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -196,24 +239,37 @@ function pp.net.formatSublevelData()
 
     local dat = pp.net.myData
     
-    dat.sl       = {}
-    dat.sl.lp    = dat.sublevel.getLogicalPose
-    dat.sl.pos   = dat.sublevel.getLogicalPose.position
-    dat.sl.quat  = dat.sublevel.getLogicalPose.orientation
-    dat.sl.mass  = dat.sublevel.getMass
-    dat.sl.pitch, 
-    dat.sl.yaw, 
-    dat.sl.roll  = dat.sublevel.getLogicalPose.orientation:toEuler()
+    dat.sl          = {}
+    dat.sl.lp       = dat.sublevel.getLogicalPose
+    dat.sl.posTrue  = pp.tVec(dat.sublevel.getLogicalPose.position)
+    dat.sl.quatTrue = dat.sublevel.getLogicalPose.orientation
+    dat.sl.mass     = dat.sublevel.getMass
+
+    dat.sl.pitchTrue, 
+    dat.sl.yawTrue, 
+    dat.sl.rollTrue = dat.sl.quatTrue:toEuler()
 
     if dat.origin then
         
-        pp.print("PPNETORIGIN!!!! ",pp.net.origin)
-        pp.net.origin = dat
+        -- pp.print("PPNETORIGIN!!!! ")
+        -- pp.net.origin = dat
+        pp.net.updateGlobalKey("origin", dat)
+        -- pp.print(pp.net.origin)
+
+        dat.sl.localPos     = vector.new()
+        dat.sl.localQuat    = quaternion.new()
+        -- pp.print(dat.sl.localQuat)
 
     elseif pp.net.origin then
 
-        pp.print("PPNETORIGIN!!!! ",pp.net.origin)
-        -- dat.sl.localquat    = 
+        local ori = pp.net.origin
+
+        -- pp.print("orisl: ",ori.sl.pos)
+        dat.sl.pos     = dat.sl.pos - pp.tVec(ori.sl.pos)
+        dat.sl.quat    = dat.sl.quat:conjugate() * pp.tQuat(ori.sl.quat)
+        dat.sl.pitch,
+        dat.sl.yaw,
+        dat.sl.roll    = dat.sl.quat:toEuler()
 
     end
     
